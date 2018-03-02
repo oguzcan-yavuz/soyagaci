c2eec73373cbddd81f0b9e8b74e360fd03bd1bbc
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2018-03-02 19:50:40 +0300

    Anne ve baba taraflarinda en cok ikamet edilen sehirler eklendi.

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,64 @@
+function mode(array)
+{
+    if(array.length === 0)
+        return null;
+    var modeMap = {};
+    var maxEl = array[0], maxCount = 1;
+    for(var i = 0; i < array.length; i++)
+    {
+        var el = array[i];
+        if(modeMap[el] === null)
+            modeMap[el] = 1;
+        else
+            modeMap[el]++;
+        if(modeMap[el] > maxCount)
+        {
+            maxEl = el;
+            maxCount = modeMap[el];
+        }
+    }
+    return maxEl;
+}
+
 $(document).ready(function () {
     // anne ve baba taraflarinin yaslarini dizilere koy
-    var anneTarafi = [];
-    var babaTarafi = [];
+    var aTarafiYas = [];
+    var aTarafiSehir = [];
+    var bTarafiYas = [];
+    var bTarafiSehir = [];
     var keysArr = Object.keys(gelen["details"]);
     for(var i = 0; i < keysArr.length; i++) {
         var arr = gelen["details"][keysArr[i]];
+        var sehir = arr[6].split("/")[0];
         var age = arr[arr.length - 1];
         if(age === 0 || age > 113) {        // buraya yas kontrolu koysak?
             continue;
         }
         if(keysArr[i].startsWith("A")) {
-            anneTarafi.push(age);
+            aTarafiYas.push(age);
+            aTarafiSehir.push(sehir);
         } else if(keysArr[i].startsWith("B")) {
-            babaTarafi.push(age);
+            bTarafiYas.push(age);
+            bTarafiSehir.push(sehir);
         }
     }
     function sum(a, b) { return a + b }
-    console.log("anne tarafi: ", anneTarafi);
-    var anneTarafiEnYasli = Math.max.apply(Math, anneTarafi);
-    var anneTarafiEnGenc = Math.min.apply(Math, anneTarafi);
+    console.log("anne tarafi: ", aTarafiYas);
+    var anneTarafiEnYasli = Math.max.apply(Math, aTarafiYas);
+    var anneTarafiEnGenc = Math.min.apply(Math, aTarafiYas);
     console.log("anne tarafi en yasli: ", anneTarafiEnYasli);
     console.log("anne tarafi en genc: ", anneTarafiEnGenc);
-    var anneTarafiOrt = (anneTarafi.reduce(sum) / anneTarafi.length).toFixed(1);
-    console.log("anne tarafi ortalama yas: ", anneTarafiOrt);
-    console.log("baba tarafi: ", babaTarafi);
-    var babaTarafiEnYasli = Math.max.apply(Math, babaTarafi);
-    var babaTarafiEnGenc = Math.min.apply(Math, babaTarafi);
-    console.log("baba tarafi en yasli", babaTarafiEnYasli);
-    console.log("baba tarafi en genc: ", babaTarafiEnGenc);
-    var babaTarafiOrt = (babaTarafi.reduce(sum) / babaTarafi.length).toFixed(1);
-    console.log("baba tarafi ortalama yas: ", babaTarafiOrt);
+    var aTarafiYasOrt = (aTarafiYas.reduce(sum) / aTarafiYas.length).toFixed(1);
+    console.log("anne tarafi ortalama yas: ", aTarafiYasOrt);
+    console.log("baba tarafi: ", bTarafiYas);
+    var bTarafiYasEnYasli = Math.max.apply(Math, bTarafiYas);
+    var bTarafiYasEnGenc = Math.min.apply(Math, bTarafiYas);
+    console.log("baba tarafi en yasli", bTarafiYasEnYasli);
+    console.log("baba tarafi en genc: ", bTarafiYasEnGenc);
+    var bTarafiYasOrt = (bTarafiYas.reduce(sum) / bTarafiYas.length).toFixed(1);
+    console.log("baba tarafi ortalama yas: ", bTarafiYasOrt);
+    console.log("anne tarafi en cok yasanan sehir: ", mode(aTarafiSehir));
+    console.log("baba tarafi en cok yasanan sehir: ", mode(bTarafiSehir));
 
     /* Pdf Yukleme */
     $('#upload form input').change(function () {
