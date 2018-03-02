1b6954c42d80d580cf2ce0f5f3201f35c608445a
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2018-03-02 14:56:33 +0300

    anne tarafi ve baba tarafinin yaslari ayri dizilerde bir araya getirildi.

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,24 @@
 /* Pdf Yukleme */
 $(document).ready(function () {
-    console.log(Object.keys(gelen["details"]));
-    var anneTarafi = 0;
-    var babaTarafi = 0;
-    for(var key in Object.keys(gelen["details"])) {
-        if(key.startsWith("A")) {
-
+    // anne ve baba taraflarinin yaslarini dizilere koy
+    var anneTarafi = [];
+    var babaTarafi = [];
+    var keysArr = Object.keys(gelen["details"]);
+    for(var i = 0; i < keysArr.length; i++) {
+        var arr = gelen["details"][keysArr[i]];
+        var age = arr[arr.length - 1];
+        if(age === 0 || age > 113) {        // buraya yas kontrolu koysak?
+            continue;
+        }
+        if(keysArr[i].startsWith("A")) {
+            anneTarafi.push(age);
+        } else if(keysArr[i].startsWith("B")) {
+            babaTarafi.push(age);
         }
     }
+    console.log(anneTarafi);
+    console.log(babaTarafi);
+
     $('#upload form input').change(function () {
         $('#upload form p').text("Lütfen aşağıdaki 'Soy Ağacımı Göster' butonuna tıklayınız.");
     });
